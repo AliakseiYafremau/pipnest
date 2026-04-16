193e0b4cf834789e13e76ac28161973c7cb516d9
AliakseiYafremau <aleksey.yafremau@gmail.com>
2026-04-16 09:52:54 +0100

    Add basic ui

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/Rotlerxd/pipnest/internal/service"
+	"github.com/Rotlerxd/pipnest/internal/ui"
+)
+
+func main() {
+	pythonPath := flag.String("python", "python", "path to python executable (used to detect backends)")
+	flag.Parse()
+
+	svc, err := service.NewService(*pythonPath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed to initialize service:", err)
+		os.Exit(1)
+	}
+
+	m := ui.NewAppModel(svc)
+
+	p := tea.NewProgram(m)
+	if err := p.Start(); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to start TUI:", err)
+		os.Exit(2)
+	}
+}
